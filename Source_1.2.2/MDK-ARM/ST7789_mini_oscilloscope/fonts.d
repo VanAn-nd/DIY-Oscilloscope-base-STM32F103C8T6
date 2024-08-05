a3e38a435c160c0e6ed249c80af5e749c8a9cfbe
Bui Van An <fasffgoo@gmail.com>
2024-08-05 23:31:15 +0700

    Add files via upload

---
+++ Source_1.2.2/MDK-ARM/ST7789_mini_oscilloscope/fonts.d
@@ -0,0 +1,3 @@
+st7789_mini_oscilloscope\fonts.o: ..\Core\Src\fonts.c
+st7789_mini_oscilloscope\fonts.o: ../Core/Inc/fonts.h
+st7789_mini_oscilloscope\fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
