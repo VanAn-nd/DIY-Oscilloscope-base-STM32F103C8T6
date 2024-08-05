8b625d7f092cb68e1c73191265fc7dab2cb5f30d
Bui Van An <fasffgoo@gmail.com>
2024-08-05 23:28:55 +0700

    Add files via upload

---
+++ Source_1.2.2/MDK-ARM/RTE/_ST7789_mini_oscilloscope/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ST7789_mini_oscilloscope' 
+ * Target:  'ST7789_mini_oscilloscope' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
